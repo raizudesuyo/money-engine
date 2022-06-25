2054c6f5a032b731fb1fd9d45e49476c9799be03
Jerome <jerome.v.angeles@gmail.com>
2022-06-25 20:48:53 +0800

    Fix missing modules

--- packages/oracle-watcher/src/nest/pricesource/pricesource.module.ts
+++ packages/oracle-watcher/src/nest/pricesource/pricesource.module.ts
@@ -1,10 +1,11 @@
+import { MoneyEngineModule } from '@money-engine/common-nest';
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database';
 import { PricesourceService } from './pricesource.service';
 
 @Module({
   providers: [PricesourceService],
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, MoneyEngineModule],
   exports: [PricesourceService]
 })
 export class PricesourceModule {}
