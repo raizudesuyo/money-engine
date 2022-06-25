46f0b422428034e870cf280e621a78b84aebd705
Jerome <jerome.v.angeles@gmail.com>
2022-06-25 21:49:33 +0800

    Remove unnecesarry IPriceSourceCurveLatestRoundDataResponse empty object alias

--- packages/common/src/adapters/oracles/IPriceSourceAdapter.ts
+++ packages/common/src/adapters/oracles/IPriceSourceAdapter.ts
@@ -12,7 +12,6 @@ export type IPriceSourceOrdinaryLatestRoundDataResponse = {
   answeredInRound: BigNumber;
 }
 
-export type IPriceSourceCurveLatestRoundDataResponse = {
-} & BigNumber
+export type IPriceSourceCurveLatestRoundDataResponse = BigNumber
 
 export type LatestRoundDataResponse = IPriceSourceOrdinaryLatestRoundDataResponse | IPriceSourceCurveLatestRoundDataResponse
